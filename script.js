c0ac3a534fce9a218a1915ba839321c8fff81c0b
Syamsul Bachri <slee17an@gmail.com>
2026-03-15 02:39:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,55 @@
-fetch("data.json")
+fetch("config.json")
+
 .then(res => res.json())
+
 .then(data => {
 
 document.getElementById("couple").innerHTML =
 data.groom + " & " + data.bride
 
-document.getElementById("date").innerHTML = data.date
+document.getElementById("heroCouple").innerHTML =
+data.groom + " & " + data.bride
 
-document.getElementById("location").innerHTML = data.location
+document.getElementById("story").innerHTML =
+data.story
 
-document.getElementById("maps").href = data.maps
+document.getElementById("date").innerHTML =
+data.date + " | " + data.time
 
-document.getElementById("music").src = data.music
+document.getElementById("venue").innerHTML =
+data.venue
+
+document.getElementById("maps").href =
+data.maps
+
+document.getElementById("wa").href =
+"https://wa.me/" + data.whatsapp
+
+document.getElementById("music").src =
+data.music
+
+
+const gallery = document.getElementById("gallery")
+
+data.gallery.forEach(img => {
+
+gallery.innerHTML +=
+"<img src='"+img+"'>"
 
 })
+
+})
+
 
 const params = new URLSearchParams(window.location.search)
 
 document.getElementById("guest").innerHTML =
 params.get("to") || "Tamu Undangan"
 
+
 function openInvite(){
 
-document.querySelector(".cover").style.display = "none"
+document.getElementById("cover").style.display="none"
 
 document.getElementById("music").play()
 
