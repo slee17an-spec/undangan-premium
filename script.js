7ffbf9a0f73f091ceffe32b1de619980b5aa9f6c
Syamsul Bachri <slee17an@gmail.com>
2026-03-15 03:16:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -110,3 +110,29 @@ flower.remove()
 }
 
 setInterval(createFlower,500)
+
+const weddingDate = new Date("July 12, 2026 09:00:00").getTime()
+
+setInterval(()=>{
+
+const now = new Date().getTime()
+
+const distance = weddingDate - now
+
+const days = Math.floor(distance/(1000*60*60*24))
+
+const hours = Math.floor((distance%(1000*60*60*24))/(1000*60*60))
+
+const minutes = Math.floor((distance%(1000*60*60))/(1000*60))
+
+const seconds = Math.floor((distance%(1000*60))/1000)
+
+document.getElementById("days").innerHTML = days
+
+document.getElementById("hours").innerHTML = hours
+
+document.getElementById("minutes").innerHTML = minutes
+
+document.getElementById("seconds").innerHTML = seconds
+
+},1000)
