d6f38e38a3cf8947bd298f64ef295fae1de2c69e
Syamsul Bachri <slee17an@gmail.com>
2026-03-16 22:04:57 +0800

    Update script.js

--- aziza&kifli/script.js
+++ aziza&kifli/script.js
@@ -39,21 +39,3 @@ navigator.clipboard.writeText("082211864752")
 alert("Nomor DANA berhasil disalin")
 
 }
-
-// ambil nama tamu dari url
-const params = new URLSearchParams(window.location.search)
-const guest = params.get("to")
-
-if(guest){
-document.querySelector(".guest").innerText = guest
-}
-
-function goSection(n){
-
-const sections = document.querySelectorAll(".page")
-
-sections[n].scrollIntoView({
-behavior:"smooth"
-})
-
-}
