934dce39c7e670f3f7b653887538e6150f1ec8b5
Syamsul Bachri <slee17an@gmail.com>
2026-03-25 00:39:18 +0700

    Delete wafiah&asiza directory

--- wafiah&asiza/script.js
+++
@@ -1,8 +0,0 @@
-const params = new URLSearchParams(window.location.search);
-const guest = params.get("to") || "Tamu Undangan";
-document.getElementById("guest").innerText = guest;
-
-document.getElementById("openBtn").onclick = () => {
-document.getElementById("opening").style.display="none";
-document.getElementById("music").play();
-};
